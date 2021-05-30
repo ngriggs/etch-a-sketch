ffd60e17b251b959718b0497056db371cb918aa5
ngriggs <noahg@vt.edu>
2021-05-30 13:58:09 -0500

    added some styling and handling for edge cases where the user enters some number higher than 64 or lower than 2 or not a number

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
 const container = document.querySelector('.container');
-
 const gridDiv = document.createElement('div');
+const resetButton = document.querySelector('.reset')
+let cells = document.querySelectorAll('.cell')
 gridDiv.classList.add('grid')
 container.appendChild(gridDiv)
 
+window.addEventListener("load", setDefaultGrid);
+resetButton.addEventListener("click", changeSize);
 
-for (i=0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', i.toString())
-    div.classList.add('cell')
-    div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'blue';
-    });
-    gridDiv.appendChild(div)
-}
-
-
-const button = document.querySelector('.reset')
-button.addEventListener('click', () => {
-    let cells = document.querySelectorAll('.cell')
-    cells.forEach((cell) => {
-        cell.style.backgroundColor = 'white';
-    });
-
-    let playerSelection = prompt("Please enter the number of squares you want to add");
-
-    if (playerSelection > 100) {
-        playerSelection = 100;
-    }
-
-    var node = document.querySelector('.grid');
-    node.querySelectorAll('*').forEach(n => n.remove());
-
-    for (i=0; i < playerSelection*playerSelection; i++) {
+function fillGrid(size) {
+    for (i = 0; i < size**2; i++) {
         const div = document.createElement('div');
-        div.setAttribute('id', i.toString())
         div.classList.add('cell')
         div.addEventListener('mouseover', (e) => {
-            /*e.target.style.backgroundColor = 'blue';*/
-
-            e.target.style.backgroundColor = Math.floor(Math.random()*16777215).toString(16);
-
+            e.target.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
         });
         gridDiv.appendChild(div)
     }
-    let newSize = 80 * 16 / playerSelection
-    gridDiv.style.gridTemplateColumns = "repeat("+playerSelection+", "+newSize+"px)";
-    gridDiv.style.gridTemplateRows = "repeat("+playerSelection+", "+newSize+"px)";
+}
 
-});
+function setDefaultGrid() {
+    fillGrid(16);
+    setGridSize(16)
+}
+function clearGrid() {
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+    });
+}
+function emptyGrid() {
+    var node = document.querySelector('.grid');
+    node.querySelectorAll('*').forEach(n => n.remove());
+}
+function setGridSize(size) {
+    gridDiv.style.gridTemplateColumns = "repeat("+size+", 1fr";
+    gridDiv.style.gridTemplateRows = "repeat("+size+", 1fr";
+}
+
+function changeSize() {
+    clearGrid()
+    let playerSelection = prompt("Please enter the number of squares you want to add");
+
+    if (playerSelection > 64 || isNaN(playerSelection) || playerSelection < 2) {
+        alert("Enter a number from 1-64 range");
+        changeSize();
+    } else {
+        emptyGrid()
+        fillGrid(playerSelection)
+        setGridSize(playerSelection)
+    }
+}
+
+
